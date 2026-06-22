bf840b1bc38c80d8a4242af6533a9a3a5eb293f1
Ansiyakn <ansiyakn786@gmail.com>
2026-06-22 10:39:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+ function greet(){
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    alert(`Name: ${name}\nEmail: ${email}`);
+ }
